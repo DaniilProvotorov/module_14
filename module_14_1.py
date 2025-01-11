5ca395a70a552d150e7331f588d50b25fc358d2a
DaniilProvotorov <dantcsar@gmail.com>
2025-01-11 19:25:46 +0500

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -15,12 +15,15 @@ balance INTEGER NOT NULL
 #for i in range(10):
 #    cursor.execute('''INSERT INTO Users (username, age, email, balance) VALUES (?, ?, ?, ?)''', (f'User{i+1}', f'{(i+1)*10}', f'example{i+1}@gmail.com', '1000'))
 
-#cursor.execute('''UPDATE Users SET balance = ? WHERE balance = 1000''', ('500',))
+# a = 1
+# for i in range(10):
+#     cursor.execute('''UPDATE Users SET balance = ? WHERE id = ?''', ('500', a))
+#     a += 2
 
-n = 1
-for i in range(10):
-    cursor.execute('''DELETE FROM Users WHERE id = ?''', (f'{n}',))
-    n += 3
+# n = 1
+# for i in range(10):
+#     cursor.execute('''DELETE FROM Users WHERE id = ?''', (f'{n}',))
+#     n += 3
 
 cursor.execute('''SELECT username, email, age, balance FROM Users WHERE age != ?''', ('60',))
 users = cursor.fetchall()
