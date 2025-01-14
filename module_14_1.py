1fd8cfd7e33f669d6a99067968bf4a27f77de55d
DaniilProvotorov <dantcsar@gmail.com>
2025-01-14 15:58:55 +0500

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -16,12 +16,12 @@ balance INTEGER NOT NULL
 #    cursor.execute('''INSERT INTO Users (username, age, email, balance) VALUES (?, ?, ?, ?)''', (f'User{i+1}', f'{(i+1)*10}', f'example{i+1}@gmail.com', '1000'))
 
 # a = 1
-# for i in range(10):
+# for i in range(1, 11, 2):
 #     cursor.execute('''UPDATE Users SET balance = ? WHERE id = ?''', ('500', a))
 #     a += 2
 
 # n = 1
-# for i in range(10):
+# for i in range(1, 11, 3):
 #     cursor.execute('''DELETE FROM Users WHERE id = ?''', (f'{n}',))
 #     n += 3
 
