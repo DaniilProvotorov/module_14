d7e3c29923afe18ca315b6fcc372dd6643da7076
DaniilProvotorov <dantcsar@gmail.com>
2025-01-16 14:57:59 +0500

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -3,6 +3,7 @@ import sqlite3
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
+
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users(
 id INTEGER PRIMARY KEY,
@@ -12,18 +13,20 @@ age INTEGER,
 balance INTEGER NOT NULL
 )
 ''')
-#for i in range(10):
-#    cursor.execute('''INSERT INTO Users (username, age, email, balance) VALUES (?, ?, ?, ?)''', (f'User{i+1}', f'{(i+1)*10}', f'example{i+1}@gmail.com', '1000'))
 
-# a = 1
-# for i in range(1, 11, 2):
-#     cursor.execute('''UPDATE Users SET balance = ? WHERE id = ?''', ('500', a))
-#     a += 2
+cursor.execute('''DELETE FROM Users ''')
 
-# n = 1
-# for i in range(1, 11, 3):
-#     cursor.execute('''DELETE FROM Users WHERE id = ?''', (f'{n}',))
-#     n += 3
+for i in range(10):
+    cursor.execute('''INSERT INTO Users (username, age, email, balance) VALUES (?, ?, ?, ?)''', (f'User{i+1}', f'{(i+1)*10}', f'example{i+1}@gmail.com', '1000'))
+
+
+for j in range(1, 11, 2):
+    cursor.execute('''UPDATE Users SET balance = ? WHERE id = ? ''', ('500', j))
+
+
+for k in range(1, 11, 3):
+    cursor.execute('''DELETE FROM Users WHERE id = ?''', (f'{k}',))
+
 
 cursor.execute('''SELECT username, email, age, balance FROM Users WHERE age != ?''', ('60',))
 users = cursor.fetchall()
